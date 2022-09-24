55d979aa2f1d4c4076c76dad031f6d2c68eb716e
RG-MISLANG <88920989+RG-MISLANG@users.noreply.github.com>
2022-09-24 12:22:20 +0000

    Added Comments
    for easier understanding of the code

--- getIPAddress.py
+++ getIPAddress.py
@@ -1,8 +1,16 @@
+#import get function from the requests library
+#requests library in order to get API from the website 
 from requests import get
 
+#run API from ipify.org and convert results to text
+#API takes Public IPv4 Address of Computer
 ip = get('https://api.ipify.org').text
+
+#print Public IPv4 Address
 print('My public IP address is: {}'.format(ip))
 
-#loc = get('https://ipapi.co/'+ip+'/json/')
-loc = get('https://ipapi.co/112.207.104.99/json/')
+#run API from ipapi.co
+#API takes an input of an IPv4 Address and gets information
+#based off of it
+loc = get('https://ipapi.co/'+ip+'/json/')
 print (loc.json())
